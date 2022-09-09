657be79af0c1d1da6298255c0f9c8ca53b145208
karineunderwood <99226176+karineunderwood@users.noreply.github.com>
2022-09-09 15:15:48 -0700

    Time: 307 ms (23.98%), Space: 15.2 MB (48.68%) - LeetHub

--- 724-find-pivot-index/724-find-pivot-index.py
+++ 724-find-pivot-index/724-find-pivot-index.py
@@ -1,6 +1,15 @@
 class Solution:
     def pivotIndex(self, nums: List[int]) -> int:
         
+        
+#         Pseudocode:
+# variable for left at 0 and right the sum of the list
+# iterate through the list in a loop
+# set the right sum mines the element
+# then check if left is equal to right, if it is return the index
+# if it is not them increase the left count + the element
+# if this condition doenst match then return -1
+
         leftSum, rightSum = 0, sum(nums)
         
         for idx, ele in enumerate(nums):
