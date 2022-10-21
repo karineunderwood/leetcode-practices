6100b40c1b5f7f93dd608de8495d4ef356631adb
karineunderwood <99226176+karineunderwood@users.noreply.github.com>
2022-10-21 13:36:05 -0700

    Time: 79 ms (47.77%), Space: 14.1 MB (26.71%) - LeetHub

--- 2185-counting-words-with-a-given-prefix/2185-counting-words-with-a-given-prefix.py
+++ 2185-counting-words-with-a-given-prefix/2185-counting-words-with-a-given-prefix.py
@@ -3,8 +3,8 @@ class Solution:
         compare = len(pref)
         counter = 0
         
-        for i in range(len(words)):
-            if words[i].startswith(pref):
+        for i in words:
+            if i[:compare] == pref:
                 counter += 1
         return counter
         
